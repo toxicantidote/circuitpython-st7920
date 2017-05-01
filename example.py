ad3de217a72d685c8ec2e6f51c2a919cfb4e38d6
Cefn Hoile <github.com@cefn.com>
2017-05-01 12:51:48 +0100

    Simplified hardware/software test cases

--- example.py
+++ example.py
@@ -2,19 +2,20 @@ from machine import SPI, Pin
 import st7920
 
 
-def run():
-    # these are the standard hardware SPI pins
+def hardware():
+    return SPI(1, baudrate=1800000, polarity=1, phase=0)
+
+def software():
+    # create a software SPI using the same pins as hardware SPI
     sck = Pin(14, mode=Pin.OUT)  # labelled 5 on nodeMCU
     mosi = Pin(13, mode=Pin.OUT)  # labelled 7 on nodeMCU
     miso = Pin(12, mode=Pin.IN)  # labelled 6 on nodeMCU # not connected, screen has no MISO line
+    return SPI(-1, baudrate=1800000, polarity=1, phase=0, sck=sck, mosi=mosi, miso=miso)
 
-    # create a software SPI using the same pins as hardware SPI
-    spi=SPI(-1, baudrate=1800000, polarity=1, phase=0, sck=sck, mosi=mosi, miso=miso)
-    # create a hardware SPI
-    #spi = SPI(1, baudrate=1800000, polarity=1, phase=0)
+def run():
 
     # create a screen using the spi
-    screen = st7920.Screen(spi=spi, slaveSelectPin=Pin(15))
+    screen = st7920.Screen(spi=hardware(), slaveSelectPin=Pin(15))
 
     # draw some points, lines, rectangles, filled rectangles in the buffer
     screen.plot(5, 5)
