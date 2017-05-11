942fb07b8b53e8d1fdb2e3d205916e498db2778a
Cefn Hoile <github.com@cefn.com>
2017-05-11 16:31:31 +0100

    Removed color references, now exclusively booleans. Set defaults to paint in black (assuming white canvas)

--- canvas.py
+++ canvas.py
@@ -1,30 +1,20 @@
-white = "white"
-black = "black"
-
 def raiseError(*a, **k):
     raise NotImplementedError()
 
 class Canvas:
 
+    width=128
+    height=64
     clear = raiseError
     plot = raiseError
     redraw = raiseError
 
-    def normalise_color(self, color):
-        if color == white:
-            color = 1
-        elif color == black:
-            color = 0
-        assert color == 0 or color == 1, "Unrecognised color specification"
-        return color
-
-    def create_plotter(self, color=black):
-        color = self.normalise_color(color)
+    def create_plotter(self, set=False):
         def plot(x,y):
-            self.plot(x,y,color)
+            self.plot(x, y, set)
         return plot
 
-    def line(self, x1, y1, x2, y2, set=True):
+    def line(self, x1, y1, x2, y2, set=False):
         diffX = abs(x2 - x1)
         diffY = abs(y2 - y1)
         shiftX = 1 if (x1 < x2) else -1
@@ -44,11 +34,11 @@ class Canvas:
                 err += diffX
                 y1 += shiftY
 
-    def fill_rect(self, x1, y1, x2, y2, set=True):
+    def fill_rect(self, x1, y1, x2, y2, set=False):
         for y in range(y1, y2 + 1):
             self.line(x1, y, x2, y, set)
 
-    def rect(self, x1, y1, x2, y2, set=True):
+    def rect(self, x1, y1, x2, y2, set=False):
         self.line(x1, y1, x2, y1, set)
         self.line(x2, y1, x2, y2, set)
         self.line(x2, y2, x1, y2, set)
