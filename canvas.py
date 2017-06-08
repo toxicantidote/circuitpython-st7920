0210adecaf31c938e79d5d5233a1a3f7da017e36
Cefn Hoile <github.com@cefn.com>
2017-06-08 12:39:38 +0100

    Resolved inconsistent logic between emulator and real screen. Set now means black.

--- canvas.py
+++ canvas.py
@@ -9,12 +9,12 @@ class Canvas:
     plot = raiseError
     redraw = raiseError
 
-    def create_plotter(self, set=False):
+    def create_plotter(self, set=True):
         def plot(x,y):
             self.plot(x, y, set)
         return plot
 
-    def line(self, x1, y1, x2, y2, set=False):
+    def line(self, x1, y1, x2, y2, set=True):
         diffX = abs(x2 - x1)
         diffY = abs(y2 - y1)
         shiftX = 1 if (x1 < x2) else -1
@@ -34,11 +34,11 @@ class Canvas:
                 err += diffX
                 y1 += shiftY
 
-    def fill_rect(self, x1, y1, x2, y2, set=False):
+    def fill_rect(self, x1, y1, x2, y2, set=True):
         for y in range(y1, y2 + 1):
             self.line(x1, y, x2, y, set)
 
-    def rect(self, x1, y1, x2, y2, set=False):
+    def rect(self, x1, y1, x2, y2, set=True):
         self.line(x1, y1, x2, y1, set)
         self.line(x2, y1, x2, y2, set)
         self.line(x2, y2, x1, y2, set)
